f83fd8b574e01508886b7e7f594b7b29bc43e3d6
Osiris8 <miganosiris8@gmaaail.com>
2023-10-23 12:08:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Effectuer une requête au backend
+    fetch(process.env.REACT_APP_BACKEND_URL)
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{message ? message : "Loading..."}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
